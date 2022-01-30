6fcb78783fe649cfc6390080234b09cf34728096
mahdi-laptop <imani.mahdi@gmail.com>
2022-01-30 16:34:17 +0330

    feat: modular similarity finder

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # NLP find similarity of two text
 
-# Preprocessing
+# A) Preprocessing and dictionary creation
 
 ```bash
-python3 farsi-preprocessing.py > output.txt
+python3 farsi-preprocessing.py > dataset/<TOPIC>-preproccesed.txt
+```
+
+# B) Find similarity in `<TOPIC>.txt` file
+
+```bash
+python3 similarity-gensim.py > <TOPIC>-result.txt
 ```
--- similarity.py
+++ similarity.py
@@ -1,6 +1,10 @@
+from gensim import corpora, models, similarities
+import jieba
+
 from gensim.parsing.preprocessing import preprocess_string, lower_to_unicode, preprocess_documents, remove_stopwords, remove_short_tokens, split_alphanum, strip_multiple_whitespaces, strip_tags
-from gensim.utils import deaccent, any2unicode, decode_htmlentities, tokenize, trim_vocab_by_freq
+from gensim.utils import deaccent, decode_htmlentities, tokenize, trim_vocab_by_freq
 from hazm import *
+from numpy import outer
 
 f = open("dataset/marketing.txt", "r")
 # f = open("dataset/example.txt", "r")
@@ -10,9 +14,9 @@ dataset = f.read()
 f = open("farsi-stopword.txt", "r")
 STOP_WORDS = f.read().split()
 
-def farsi_preprocessing(rawDocument):
+def FarsiPreprocessing(rawDocument):
     tokenizedDocument = rawDocument.splitlines()
-
+    output = ''
     for i in range(len(tokenizedDocument)):
         if tokenizedDocument[i] is not "":
 
@@ -40,7 +44,35 @@ def farsi_preprocessing(rawDocument):
             tmp = normalizer.normalize(tokenizedDocument[i])
             tmp = stemmer.stem(tmp)
             tmp = lemmatizer.lemmatize(tmp)
-            print(tmp)
+            # print(tmp)
+            output += '\n' + tmp
+    return output
 
 
-farsi_preprocessing(dataset)
+def findSimilarity(keyword: str, texts: str, matchPercentage: float, dictionary):
+    dictionary = corpora.Dictionary(dictionary)
+    feature_cnt = len(dictionary.token2id)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    tfidf = models.TfidfModel(corpus)
+    kw_vector = dictionary.doc2bow(jieba.lcut(keyword))
+    index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features = feature_cnt)
+    sim = index[tfidf[kw_vector]]
+
+    for i in range(len(sim)):
+        if sim[i] > matchPercentage:
+            print('==== %.2f ===' % sim[i])
+            print(dataset[i])
+
+
+if __name__ == "__main__":
+    keyword = 'مشتری توزیع کننده خرده فروش است'
+
+    f = open("dataset/marketing-preprocced.txt", "r")
+    dictionary = FarsiPreprocessing(f.read())
+    dictionary = [jieba.lcut(text) for text in dictionary.splitlines()]
+
+    f = open("dataset/marketing.txt", "r")
+    dataset = f.readlines()
+    texts = [jieba.lcut(text) for text in dataset]
+
+    findSimilarity(keyword, texts, 0.8, dictionary)
